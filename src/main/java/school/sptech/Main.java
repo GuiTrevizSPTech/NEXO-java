5cb9a36690f0fed13810bed9511f865cb482121c
GuiTrevizSPTech <guilherme.conceicao@sptech.school>
2025-09-05 17:42:40 -0300

    fix: Adicionando mais objetos para Log

--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -4,9 +4,19 @@ public class Main {
     public static void main(String[] args) {
         AdministradorLog logGerenciador = new AdministradorLog();
 
-        logGerenciador.adicionarLog(new Log("Totem0011A","2025-09-04 19:00", 30.5, 50.2, 70.1));
-        logGerenciador.adicionarLog(new Log("Totem0012A","2025-09-04 19:10", 80.1, 60.5, 55.2));
-        logGerenciador.adicionarLog(new Log("Totem0013A","2025-09-04 19:05", 45.8, 30.0, 90.3));
+        logGerenciador.adicionarLog(new Log("Totem0011A","2025-09-04 19:00", 82.5, 50.2, 70.1));
+        logGerenciador.adicionarLog(new Log("Totem0011A","2025-09-04 19:10", 76.5, 54.2, 78.6));
+        logGerenciador.adicionarLog(new Log("Totem0011A","2025-09-04 19:05", 80.1, 60.5, 54.2));
+        logGerenciador.adicionarLog(new Log("Totem0011A","2025-09-04 19:20", 45.8, 30.0, 90.3));
+        logGerenciador.adicionarLog(new Log("Totem0012A","2025-09-04 19:00", 80.1, 60.5, 75.2));
+        logGerenciador.adicionarLog(new Log("Totem0012A","2025-09-04 19:10", 25.6, 60.5, 77.2));
+        logGerenciador.adicionarLog(new Log("Totem0012A","2025-09-04 19:05", 32.1, 60.5, 59.2));
+        logGerenciador.adicionarLog(new Log("Totem0012A","2025-09-04 19:00", 40.1, 60.5, 55.2));
+        logGerenciador.adicionarLog(new Log("Totem0013A","2025-09-04 19:10", 45.8, 30.0, 90.9));
+        logGerenciador.adicionarLog(new Log("Totem0013A","2025-09-04 19:05", 90.8, 30.0, 90.4));
+        logGerenciador.adicionarLog(new Log("Totem0013A","2025-09-04 19:00", 94.8, 30.0, 90.6));
+        logGerenciador.adicionarLog(new Log("Totem0013A","2025-09-04 19:20", 81.7, 30.0, 90.3));
+
 
         System.out.println("=== Todos os Logs ===");
         logGerenciador.mostrarTodosLogs();
